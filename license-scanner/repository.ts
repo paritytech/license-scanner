0596ef73b9dee2b58bb16e23db195cf062e0667c
Przemyslaw Rzad <przemek@parity.io>
2023-01-05 14:19:59 +0100

    Use `opstooling-js-style` (#15)

--- license-scanner/repository.ts
+++ license-scanner/repository.ts
@@ -1,93 +1,72 @@
-import assert from "assert"
-import { basename, join as joinPath } from "path"
-import tar from "tar"
-import tmp from "tmp"
+import assert from "assert";
+import { basename, join as joinPath } from "path";
+import tar from "tar";
+import tmp from "tmp";
 
-import { Logger } from "./logger"
-import { downloadMutex } from "./synchronization"
-import { RepositoryCrate } from "./types"
-import {
-  download,
-  ensureDir,
-  existsAsync,
-  readdirAsync,
-  renameAsync,
-  unlinkAsync,
-} from "./utils"
+import { Logger } from "./logger";
+import { downloadMutex } from "./synchronization";
+import { RepositoryCrate } from "./types";
+import { download, ensureDir, existsAsync, readdirAsync, renameAsync, unlinkAsync } from "./utils";
 
 const getVersionedRepositoryName = function (crate: RepositoryCrate) {
-  return `${basename(crate.source.repository)}-${crate.source.ref.value}`
-}
+  return `${basename(crate.source.repository)}-${crate.source.ref.value}`;
+};
 
 // e.g. https://github.com/org/repo/tarball/[tag,ref,sha]
 const getRepositoryDownloadUrl = function (crate: RepositoryCrate) {
-  return `${crate.source.repository}/tarball/${crate.source.ref.value}`
-}
+  return `${crate.source.repository}/tarball/${crate.source.ref.value}`;
+};
 
-const fetchRepository = async function (
-  tmpDir: string,
-  crate: RepositoryCrate,
-  destination: string,
-  logger: Logger,
-) {
-  const tarball = `${destination}.tar`
+const fetchRepository = async function (tmpDir: string, crate: RepositoryCrate, destination: string, logger: Logger) {
+  const tarball = `${destination}.tar`;
   if (!(await existsAsync(tarball))) {
-    await downloadMutex.runExclusive(function () {
-      logger.info(
-        `Downloading repository ${crate.source.repository} (ref: ${crate.source.ref.value})`,
-      )
-      return download(getRepositoryDownloadUrl(crate), tarball)
-    })
+    await downloadMutex.runExclusive(() => {
+      logger.info(`Downloading repository ${crate.source.repository} (ref: ${crate.source.ref.value})`);
+      return download(getRepositoryDownloadUrl(crate), tarball);
+    });
   }
 
-  const extractionDir = await new Promise<{ path: string; remove: () => void }>(
-    function (resolve, reject) {
-      tmp.dir({ tmpdir: tmpDir }, function (err, path, remove) {
-        if (err === null || err === undefined) {
-          resolve({ path, remove })
-        } else {
-          reject(err)
-        }
-      })
-    },
-  )
-
-  await new Promise<void>(function (resolve, reject) {
-    tar.x({ f: tarball, cwd: extractionDir.path }, undefined, function (err) {
-      if (err === undefined) {
-        resolve()
+  const extractionDir = await new Promise<{ path: string; remove: () => void }>((resolve, reject) => {
+    tmp.dir({ tmpdir: tmpDir }, (err, path, remove) => {
+      if (err === null || err === undefined) {
+        resolve({ path, remove });
       } else {
-        reject(err)
+        reject(err);
       }
-    })
-  })
+    });
+  });
 
-  const extractedFolders = await readdirAsync(extractionDir.path)
-  assert(extractedFolders.length === 1)
-  const extractedFolder = extractedFolders[0]
-  assert(typeof extractedFolder === "string")
-  await renameAsync(joinPath(extractionDir.path, extractedFolder), destination)
+  await new Promise<void>((resolve, reject) => {
+    tar.x({ f: tarball, cwd: extractionDir.path }, undefined, (err) => {
+      if (err === undefined) {
+        resolve();
+      } else {
+        reject(err);
+      }
+    });
+  });
 
-  await unlinkAsync(tarball)
-  extractionDir.remove()
-}
+  const extractedFolders = await readdirAsync(extractionDir.path);
+  assert(extractedFolders.length === 1);
+  const extractedFolder = extractedFolders[0];
+  assert(typeof extractedFolder === "string");
+  await renameAsync(joinPath(extractionDir.path, extractedFolder), destination);
+
+  await unlinkAsync(tarball);
+  extractionDir.remove();
+};
 
 export const getOrDownloadRepository = async function (
   repositoriesDir: string,
   crate: RepositoryCrate,
   logger: Logger,
 ) {
-  const targetRefDir = await ensureDir(
-    joinPath(repositoriesDir, crate.source.ref.tag),
-  )
+  const targetRefDir = await ensureDir(joinPath(repositoriesDir, crate.source.ref.tag));
 
-  const repositoryPath = joinPath(
-    targetRefDir,
-    getVersionedRepositoryName(crate),
-  )
+  const repositoryPath = joinPath(targetRefDir, getVersionedRepositoryName(crate));
   if (!(await existsAsync(repositoryPath))) {
-    await fetchRepository(repositoriesDir, crate, repositoryPath, logger)
+    await fetchRepository(repositoriesDir, crate, repositoryPath, logger);
   }
 
-  return repositoryPath
-}
+  return repositoryPath;
+};
