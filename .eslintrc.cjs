dc5e5ede755cb88b0eec0a6349bf31b5268d9477
rzadp <roopert7@gmail.com>
2023-01-05 14:00:58 +0100

    Linting config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,1 +1,7 @@
-module.exports = require("opstooling-js-style/src/eslint/configuration")
+const { getConfiguration } = require("opstooling-js-style/src/eslint/configuration");
+
+const tsConfParams = { rootDir: __dirname };
+
+const conf = getConfiguration({ typescript: tsConfParams });
+
+module.exports = conf
