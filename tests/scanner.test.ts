62fe424aa35c3d17153fb6cd46753ee4ed33436a
rzadp <roopert7@gmail.com>
2023-01-06 17:09:24 +0100

    First failing test with multiple crates

--- tests/scanner.test.ts
+++ tests/scanner.test.ts
@@ -1,17 +1,59 @@
 import { executeScanArgs } from "license-scanner/cli/scan";
-import path from "path";
+import path, {join as joinPath} from "path";
 import { fileURLToPath } from "url";
+import {getLicenseMatcher, loadLicensesNormalized} from "../license-scanner/license";
+import {
+  cratesDir,
+  extraLicenses,
+  licenseAliases,
+  projectRoot,
+  repositoriesDir,
+  rustCrateScannerRoot
+} from "../license-scanner/constants";
+import {scan} from "../license-scanner/scanner";
+import {ScanOptions, ScanResultItem, ScanTracker} from "../license-scanner/types";
+import {Logger} from "../license-scanner/logger";
 
 describe("Scanner tests", () => {
-  let targetsRoot: string;
-  before(() => {
-    targetsRoot = path.join(path.dirname(fileURLToPath(import.meta.url)), "./targets");
+  let scanOptions: Omit<ScanOptions, "root" | "initialRoot" | "saveResult">
+  const targetsRoot = path.join(path.dirname(fileURLToPath(import.meta.url)), "./targets");
+
+  before(async () => {
+    const licenses = await loadLicensesNormalized(joinPath(projectRoot, "..", "licenses"), {
+      aliases: licenseAliases,
+      extraLicenses,
+    });
+    scanOptions = {
+      matchLicense: getLicenseMatcher(licenses),
+      dirs: { crates: cratesDir, repositories: repositoriesDir },
+      rust: { shouldCheckForCargoLock: true, cargoExecPath: "cargo", rustCrateScannerRoot },
+      tracker: new ScanTracker(),
+      detectionOverrides: null,
+      logger: new Logger({ minLevel: 'debug' }),
+    }
   });
 
-  it("single-crate", async () => {
+  const performScan = async (target: string) => {
     const scanRoot = path.join(targetsRoot, "single-crate");
-    await executeScanArgs({
-      args: { scanRoot, startLinesExcludes: null, detectionOverrides: null, logLevel: "debug" },
+    const output: Record<string, ScanResultItem> = {}
+    await scan({
+      ...scanOptions,
+      saveResult: async (projectId, filePathFromRoot, result) => {
+        output[filePathFromRoot] = result
+      },
+      root: scanRoot,
+      initialRoot: scanRoot,
     });
-  });
+    return output
+  }
+
+  it("single-crate", async () => {
+    const output = await performScan("single-crate")
+    console.log(output)
+  })
+
+  it("multiple-crates", async () => {
+    const output = await performScan("multiple-crates")
+    console.log(output)
+  })
 });
