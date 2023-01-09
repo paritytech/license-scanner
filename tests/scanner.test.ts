bfd68a3a45231a73822fecdcdb4d3bf7632d0c37
rzadp <roopert7@gmail.com>
2023-01-09 12:40:08 +0100

    Add tests

--- tests/scanner.test.ts
+++ tests/scanner.test.ts
@@ -1,4 +1,5 @@
-import { expect } from "chai";
+import chai, { expect } from "chai";
+import chaiAsPromised from "chai-as-promised";
 import {
   cratesDir,
   extraLicenses,
@@ -14,8 +15,10 @@ import { ScanOptions, ScanTracker } from "license-scanner/types";
 import path, { join as joinPath } from "path";
 import { fileURLToPath } from "url";
 
+chai.use(chaiAsPromised);
+
 describe("Scanner tests", () => {
-  let scanOptions: Omit<ScanOptions, "root" | "initialRoot" | "saveResult" | "tracker">;
+  let commonScanOptions: Omit<ScanOptions, "root" | "initialRoot" | "saveResult" | "tracker">;
   const targetsRoot = path.join(path.dirname(fileURLToPath(import.meta.url)), "./targets");
 
   before(async () => {
@@ -23,7 +26,7 @@ describe("Scanner tests", () => {
       aliases: licenseAliases,
       extraLicenses,
     });
-    scanOptions = {
+    commonScanOptions = {
       matchLicense: getLicenseMatcher(licenses),
       dirs: { crates: cratesDir, repositories: repositoriesDir },
       rust: { shouldCheckForCargoLock: true, cargoExecPath: "cargo", rustCrateScannerRoot },
@@ -32,11 +35,11 @@ describe("Scanner tests", () => {
     };
   });
 
-  const performScan = async (target: string) => {
+  const performScan = async (target: string, overrideScanOptions?: Partial<ScanOptions>) => {
     const scanRoot = path.join(targetsRoot, target);
     const output: Record<string, { description?: string; license?: string }> = {};
     await scan({
-      ...scanOptions,
+      ...commonScanOptions,
       saveResult: async (projectId, filePathFromRoot, result) => {
         output[filePathFromRoot] = result;
         return await Promise.resolve();
@@ -44,6 +47,7 @@ describe("Scanner tests", () => {
       tracker: new ScanTracker(),
       root: scanRoot,
       initialRoot: scanRoot,
+      ...overrideScanOptions,
     });
     return output;
   };
@@ -60,5 +64,34 @@ describe("Scanner tests", () => {
     expect(output["first-crate/src/main.rs"]?.license).to.equal("Apache-2.0");
     expect(output["second-crate/LICENSE"]?.license).to.equal("UNLICENSE");
     expect(output["second-crate/src/main.rs"]?.license).to.equal("GPL-3.0-or-later");
+  });
+
+  describe("ensure license", async () => {
+    it("works when file properly licensed", async () => {
+      const output = await performScan("required-license/src/licensed", { ensureLicenses: ["Apache-2.0"] });
+      expect(output["main.rs"]?.license).to.equal("Apache-2.0");
+    });
+
+    it("throws when file not licensed", async () => {
+      await expect(
+        performScan("required-license/src/not-licensed", { ensureLicenses: ["Apache-2.0"] }),
+      ).to.eventually.be.rejectedWith("Ensuring files have license failed: No license detected in main.rs");
+    });
+
+    it("throws when file copyrighted but not licensed", async () => {
+      await expect(
+        performScan("required-license/src/copyrighted", { ensureLicenses: ["Apache-2.0"] }),
+      ).to.eventually.be.rejectedWith(
+        'Ensuring files have license failed: main.rs resulted in: Flagged because "copyright" was found somewhere within this file, but no licenses were detected',
+      );
+    });
+
+    it("throws when file licensed differently", async () => {
+      await expect(
+        performScan("required-license/src/licensed-differently", { ensureLicenses: ["Apache-2.0"] }),
+      ).to.eventually.be.rejectedWith(
+        "Ensuring files have license failed: main.rs has MIT license, expected one of: Apache-2.0",
+      );
+    });
   });
 });
