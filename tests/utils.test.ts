84ab38a09fda5f859ccecee522dcc83e7b603d13
rzadp <roopert7@gmail.com>
2023-01-10 14:27:22 +0100

    build step

--- tests/utils.test.ts
+++ tests/utils.test.ts
@@ -1,11 +1,13 @@
 import { expect } from "chai";
 import { isBinaryFile } from "license-scanner/utils";
 import path from "path";
+import { fileURLToPath } from "url";
 
 describe("Utils", () => {
   it("isBinaryFile", async () => {
-    const binaryFilePath = path.join(__dirname, "./elf-binary-file");
-    const nonBinaryFilePath = path.join(__dirname, "./utils.test.ts");
+    const dir = path.dirname(fileURLToPath(import.meta.url));
+    const binaryFilePath = path.join(dir, "./elf-binary-file");
+    const nonBinaryFilePath = path.join(dir, "./utils.test.ts");
 
     expect(await isBinaryFile(binaryFilePath)).to.be.true;
     expect(await isBinaryFile(nonBinaryFilePath)).to.be.false;
